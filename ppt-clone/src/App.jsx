11ba254ece9510048a1649c05dc25ccb79852bff
Kaushal Kishor <kaushal.kishor@niit.com>
2026-02-23 21:41:45 +0530

    more content

--- ppt-clone/src/App.jsx
+++ ppt-clone/src/App.jsx
@@ -1,15 +1,43 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Presentation from "./pages/Presentation";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <div>
+        <a href="/">Home</a>
+      </div>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/presentation/:projectId" element={<Presentation />} />
       </Routes>
+
+      {/* Floating Branding */}
+      {/* Floating Branding */}
+      <div className="fixed bottom-6 right-6 z-[9999]">
+        <div
+          className="
+    bg-white 
+    shadow-2xl 
+    rounded-2xl 
+    px-6 py-4 
+    flex flex-col 
+    text-lg 
+    font-extrabold 
+    text-gray-900 
+    border border-gray-300
+    tracking-wide
+  "
+        >
+          <span className="hover:scale-105 transition-transform duration-200 cursor-pointer">
+            @kkdevportal
+          </span>
+          <span className="hover:scale-105 transition-transform duration-200 cursor-pointer">
+            @techtrekkertales
+          </span>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
